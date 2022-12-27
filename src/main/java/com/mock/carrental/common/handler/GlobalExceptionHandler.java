8de803622e76fd5b95afec57ce35f618a63ed290
xiaosanyeshiye <191077743@qq.com>
2022-12-27 13:48:29 +0800

    delete surplus javadoc

--- src/main/java/com/mock/carrental/common/handler/GlobalExceptionHandler.java
+++ src/main/java/com/mock/carrental/common/handler/GlobalExceptionHandler.java
@@ -8,10 +8,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-/***
- * 全局异常处理
- * @author luw
- */
 @Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
